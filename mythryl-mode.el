4e2e88288d6095483c29aad18bce385c4659a151
Michele Bini <michele.bini@gmail.com>
2011-12-22 08:16:52 +0100

    Advanced to version 2.5.3

--- mythryl-mode.el
+++ mythryl-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2009 Phil Rand <philrand@gmail.com>
 ;; Copyright (C) 2010, 2011 Michele Bini <michele.bini@gmail.com> aka Rev22
 
-;; Version: 2.5.2
+;; Version: 2.5.3
 ;; Maintainer: Michele Bini <michele.bini@gmail.com>
 
 ;; mythryl.el is not part of Emacs
